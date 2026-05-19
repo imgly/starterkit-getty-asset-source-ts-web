21815851996ddb4af2305dffa1d5287aba67881b
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-05-19 16:54:43 +0200

    Update starterkit for v1.75.0

--- src/imgly/index.ts
+++ src/imgly/index.ts
@@ -9,10 +9,11 @@
  * @see https://developer.gettyimages.com/
  */
 
-import CreativeEditorSDK from '@cesdk/cesdk-js';
+import type CreativeEditorSDK from '@cesdk/cesdk-js';
 
 import {
   BlurAssetSource,
+  ImageColorsAssetSource,
   ColorPaletteAssetSource,
   CropPresetsAssetSource,
   DemoAssetSources,
@@ -81,6 +82,7 @@ export async function initGettyImagesEditor(
   await cesdk.addPlugin(new BlurAssetSource());
 
   // Color palettes for design
+  await cesdk.addPlugin(new ImageColorsAssetSource());
   await cesdk.addPlugin(new ColorPaletteAssetSource());
 
   // Crop presets (aspect ratios)
