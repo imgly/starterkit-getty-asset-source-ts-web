e056566befe0c06f3edd36363d8ed13801a32d94
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-09-18 20:18:11 +0200

    Update starterkit for v1.82.1-rc.1

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@
  * });
  * ```
  *
- * @see https://img.ly/docs/cesdk/js/getting-started/
+ * @see https://img.ly/docs/cesdk/js/get-started/overview-e18f40/
  * @see https://developer.gettyimages.com/
  */
 
@@ -30,6 +30,7 @@ import CreativeEditorSDK from '@cesdk/cesdk-js';
 
 import { initGettyImagesEditor } from './imgly';
 import { resolveAssetPath } from './imgly/resolveAssetPath';
+
 
 // ============================================================================
 // Configuration
@@ -78,7 +79,7 @@ CreativeEditorSDK.create('#cesdk_container', config)
 
     // Load the Getty Images demo scene from CDN
     // This scene showcases images that can be replaced with photos from Getty Images
-    await cesdk.loadFromURL(resolveAssetPath('/assets/getty-images.scene'));
+    await cesdk.load(resolveAssetPath('/assets/getty-images.scene'));
   })
   .catch((error) => {
     // eslint-disable-next-line no-console
